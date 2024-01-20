ae451d6f13d7e9bccfdcf5c08efdb3bd56080285
Daniyar Sabitov <hottabbych.com@yandex.ru>
2024-01-20 23:50:17 +0300

    initial

---
+++ src/RoadCamera.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RoadCamera {
+    public static void main(String[] args) {
+        //Параметры программы
+        int maxOncomingSpeed = 30; // km/h
+        int speedFineGrade = 20; // km/h
+        int finePerGrade = 500; // RUB
+        int criminalSpeed = 180; // km/h
+
+
+        System.out.println("Введите скорость автомобиля:");
+        int oncomingSpeed = new Scanner(System.in).nextInt(); // запрос с клавиатуры
+
+
+        if (oncomingSpeed >= criminalSpeed) {
+            System.out.println("Вызов полиции...");
+        } else if (oncomingSpeed > maxOncomingSpeed) {
+            int overSpeed = oncomingSpeed - maxOncomingSpeed;
+            int gradesCount = overSpeed / speedFineGrade;
+            int fine = finePerGrade * gradesCount;
+            System.out.println("Сумма штрафа: " + fine);
+        } else {
+            System.out.println("Скорость не превышена");
+        }
+    }
+}
